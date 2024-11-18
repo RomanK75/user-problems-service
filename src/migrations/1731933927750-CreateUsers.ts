ad1f0461129640bb3466c7647b3e388b4791cd4b
Mytth <Mytth75@gmail.com>
2024-11-18 20:21:23 +0700

    usersCreated

--- src/migrations/1731933927750-CreateUsers.ts
+++ src/migrations/1731933927750-CreateUsers.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsers1234567890 implements MigrationInterface {
+export class CreateUsers1731933927750 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
