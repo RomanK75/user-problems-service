ced2a24f506d63fd2b3d4de222ca08f51a09b629
Mytth <Mytth75@gmail.com>
2024-11-19 12:12:40 +0700

    final

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,11 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post('reset-problems')
+  resetProblemsAndCount() {
+    return this.usersService.resetProblemsAndCount();
+  }
+
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async resetProblemsAndCount(): Promise<{ updatedCount: number }> {
+    const result = await this.usersRepository
+      .createQueryBuilder()
+      .update(User)
+      .set({ hasProblems: false })
+      .where('hasProblems = :value', { value: true })
+      .execute();
+
+    return { updatedCount: result.affected || 0 };
+  }
+
+  
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
